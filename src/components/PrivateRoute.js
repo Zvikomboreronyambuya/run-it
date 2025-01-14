1996e12f79905a8f15c49cd6847139ae2162c661
Zvikomborero Nyambuya <znyambuya@gmail.com>
2025-01-14 09:46:54 +0200

    added function doc for Private Route component

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -2,6 +2,12 @@ import React from 'react';
 import { Outlet, Navigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
 
+/**
+ * PrivateRoute component.
+ * Renders the Outlet component if the user is authenticated, otherwise redirects to the login page.
+ *
+ * @returns {JSX.Element} The rendered component.
+ */
 const PrivateRoute = () => {
     const { isAuthenticated } = useAuth();
     
